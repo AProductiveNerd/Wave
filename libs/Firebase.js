fc8d97193674f090ab84178624ec8dc9bd6f6290
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-04-21 19:33:01 +0530

    Initial

---
+++ libs/Firebase.js
@@ -0,0 +1,17 @@
+import "firebase/firestore";
+import "firebase/auth";
+
+import Firebase from "firebase/app";
+
+const config = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const firebase = Firebase.initializeApp(config);
+const fireAuth = firebase.auth();
+export { firebase, fireAuth };
